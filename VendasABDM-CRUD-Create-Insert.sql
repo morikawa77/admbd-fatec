b0304377df53aea35f937e02b1f4bce0ec892160
Reginaldo Morikawa <morikawa77@gmail.com>
2025-03-25 11:18:19 -0300

    Aula 25032025

--- VendasABDM-CRUD-Create-Insert.sql
+++ VendasABDM-CRUD-Create-Insert.sql
@@ -126,3 +126,65 @@ go
 insert into Pedidos (data, status, vendedorId, clienteId) values (GETDATE(), 1, 2, 1004) 
 go
 
+insert into pedidos (data, status, vendedorId, clienteId)
+values	('2025-10-02', 2, 7, 6),
+		('2025-12-02', 1, 7, 8),
+		(getdate(), 1, 9, 5),
+		(getdate(), 1, 2, 5),
+		('2025-17-03', null, 2, 5)
+go
+
+-- ARRUMAR DAQUI PRA BAIXO
+-- inserir dados na tabela de itens_pedidos
+
+-- inserir itens do pedido 1
+insert into itens_pedidos (pedidoId, produtoId, qtd, valor)
+values	(1, 3, 100, 2.50),
+		(1, 8, 1, 4.50),
+		(1, 10, 5, 35.98),
+		(1, 11, 100, 15.25)
+go
+
+-- inserir itens do pedido 2
+insert into itens_pedidos (pedidoid, produtoid, qtd, valor)
+values	(2, 1, 5, 25.00),	
+		(2, 20, 5, 16.49)
+go
+
+-- inserir itens do pedido 3
+insert into itens_pedidos (pedidoid, produtoid, qtd, valor)
+values	(3, 1, 10, 24.50),
+		(3, 5, 15, 3.50),
+		(3, 9, 120, 74.50),
+		(3, 16, 2, 2.50),
+		(3, 17, 3, 12.50)
+go
+
+-- inserir itens do pedido 4
+insert into itens_pedidos (pedidoid, produtoid, qtd, valor)
+values	(4, 19, 10, 4.59)
+go
+
+-- inserir itens do pedido 5
+insert into itens_pedidos (pedidoid, produtoid, qtd, valor)
+values	(5, 14, 2, 9.80),
+		(5, 17, 1, 12.50),
+		(5, 18, 12, 6.50),
+		(5, 20, 5, 15.50)
+go
+
+-- inserir itens do pedido 7
+insert into itens_pedidos (pedidoid, produtoid, qtd, valor)
+values	(7, 2, 5, 5.50),
+		(7, 3, 10, 2.50),
+		(7, 15, 2, 15.00)
+go
+
+-- consultar itens de todos os pedidos
+select * from itens_pedidos
+go
+
+-- consultar itens pedido especifico
+select * from itens_pedidos
+where pedidoid = 2
+go
